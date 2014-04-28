d226f5a537410c048bee83e5c1b86b2afcd16dd3
XarpeSerpe <xarpeserpe@gmail.com>
2014-04-28 02:19:27 +0200

    TODO uso de espacios tras hacer char(incoming)
    Así no habrá que trabajar con equivalencias ascii, solo hay que programar bien la lectura de los espacios.

--- comando.ino
+++ comando.ino
@@ -1,4 +1,8 @@
 /*Estudio de la comunicacin UART usando los pines 0  RX y 1 TX
+
+TODO uso de char, ojo a los espacios, interpretar las entradas con if , limitar las potencias y los tiempos 3000 milisegundos.
+
+
 equivalencia ascii
 0 es 48
 1 es 49
@@ -13,6 +17,8 @@ Formato de comandos
 f250 100 adelante a 250 durante 100 milisegundos    
 b120 5000 atras a 120 durante 5000 milisegundos
 a45 mover motores a 45 grados
+d120 200 gira a 120 dextrógiro durate 200 milisegundos
+l255 500 gira levógito a 255 durante 500 milisegundos
 */
 
 int incomingByte = 0;
@@ -31,7 +37,7 @@ void loop()
   {
     incomingByte = Serial.read();
     Serial.print("He recibido ");
-    Serial.println(incomingByte, DEC);
+    Serial.println(incomingByte, DEC); // se puede trabajar en caracter si se usa Serial.println(char(incomingByte)); pero puede dar problemas con los espacios.
     if (incomingByte == 32) { digitalWrite(13, HIGH); }
     else { digitalWrite(13, LOW); }
   }
